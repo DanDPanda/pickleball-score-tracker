5b1e3476c3ee6554a21cf3fb7a891d45662ee5ac
DanDPanda <dan.v.dinh@gmail.com>
2025-12-08 16:03:14 -0600

    Adding database binding

---
+++ schema.sql
@@ -0,0 +1,9 @@
+--npx wrangler d1 execute prod-d1-tutorial --local --file=./schema.sql
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Weeks;
+DROP TABLE IF EXISTS Seasons;
+DROP TABLE IF EXISTS Scores;
+CREATE TABLE IF NOT EXISTS Users (userId TEXT PRIMARY KEY, email TEXT, facilitator BOOLEAN);
+CREATE TABLE IF NOT EXISTS Weeks (weekId TEXT PRIMARY KEY, startDate TEXT, endDate TEXT, active BOOLEAN);
+CREATE TABLE IF NOT EXISTS Seasons (seasonId TEXT PRIMARY KEY, startDate TEXT, endDate TEXT, active BOOLEAN);
+CREATE TABLE IF NOT EXISTS Scores (scoreId TEXT PRIMARY KEY, userId TEXT, weekId TEXT, seasonId TEXT, score INTEGER, FOREIGN KEY(userId) REFERENCES Users(userId), FOREIGN KEY(weekId) REFERENCES Weeks(weekId), FOREIGN KEY(seasonId) REFERENCES Seasons(seasonId));
